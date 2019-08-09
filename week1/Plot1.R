872f14a8b8d7fbede8da36f4a532855e8b861f6c
preethical <preethi@deobald.ca>
2019-08-09 12:50:34 -0300

    Plot1.R now with a functional png file

--- week1/Plot1.R
+++ week1/Plot1.R
@@ -2,15 +2,24 @@
 library(dplyr)
 power_consumption <- read.table("exdata_data_household_power_consumption/household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
 
-power_consumption$Date <- as.Date(power_consumption$Date, "%d/%m/%Y")
-
-##Power_consumption <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
 ## change date and from character vectors to time and date. 
 
+power_consumption$Date <- as.Date(power_consumption$Date, "%d/%m/%Y")
+power_consumption$Time <- format(power_consumption$Time, format= "%H:%M:%S")
 
-##power_consumption$Time <- strptime(power_consumption$Time, "%H:%M:%S")
+## Subset the required dates
+Power_subset <- subset(power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
 
+##other ways to subset dates
+##Power_consumption <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
 ##Power_subset <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
-##Power_subset <- subset(power_consumption, power_consumption$Date >= "2007-02-01" | power_consumption$Date <= "2007-02-02")
 ##Power_subset <- power_consumption %>% filter(between( Date, 2007-02-01, 2007-02-02))
 ##Sub_set <- power_consumption[(power_consumption$Date >= "2007-02-01") & (power_consumption$Date <= "2007-02-02")]
+
+png(plot1.png, width=480, height=480)
+hist(Power_subset$Global_active_power, col = "Red", border ="black", main = "Global Active Power", xlab ="Global active power(kilowatts)", ylab = "frequency")
+dev.off()
+
+
+
+
