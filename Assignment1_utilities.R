0e99401f107bf2de934b2d94bf29ce639965144d
preethical <preethi@deobald.ca>
2019-08-09 13:11:00 -0300

    An assignment 1 utilities file was created to include the arguments that are repeated through all programms. plot 2 is in progress

---
+++ Assignment1_utilities.R
@@ -0,0 +1,17 @@
+##Utility arguments that will be reused for all plots have been seperated and placed in this 
+##file and are called for each plot. 
+
+##setwd("~/R/datascience_coursera_main/Exploratory-analysis/Exploratory-Analysis_Main/week1"))
+power_consumption <- read.table("exdata_data_household_power_consumption/household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
+
+## change date and from character vectors to time and date. 
+power_consumption$Date <- as.Date(power_consumption$Date, "%d/%m/%Y")
+power_consumption$Time <- format(power_consumption$Time, format= "%H:%M:%S")
+
+## Subset the required dates
+Power_subset <- subset(power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
+##other ways to subset dates
+##Power_consumption <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
+##Power_subset <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
+##Power_subset <- power_consumption %>% filter(between( Date, 2007-02-01, 2007-02-02))
+##Sub_set <- power_consumption[(power_consumption$Date >= "2007-02-01") & (power_consumption$Date <= "2007-02-02")]
--- week1/Plot1.R
+++ week1/Plot1.R
@@ -1,14 +1,19 @@
+
+## Utility functions have been extracted into another file for reuse across 
+## the multiple files in this assignment. Please see the following utility 
+## R file to explore their contents.
+source(here::here("Assignment1_utilities.R"))
 ##setwd("~/R/datascience_coursera_main/Exploratory-analysis/Exploratory-Analysis_Main/week1")
-library(dplyr)
-power_consumption <- read.table("exdata_data_household_power_consumption/household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
+##library(dplyr)
+##power_consumption <- read.table("exdata_data_household_power_consumption/household_power_consumption.txt", na.strings = "?", sep = ";", header = TRUE)
 
 ## change date and from character vectors to time and date. 
 
-power_consumption$Date <- as.Date(power_consumption$Date, "%d/%m/%Y")
-power_consumption$Time <- format(power_consumption$Time, format= "%H:%M:%S")
+##power_consumption$Date <- as.Date(power_consumption$Date, "%d/%m/%Y")
+##power_consumption$Time <- format(power_consumption$Time, format= "%H:%M:%S")
 
 ## Subset the required dates
-Power_subset <- subset(power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
+##Power_subset <- subset(power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
 
 ##other ways to subset dates
 ##Power_consumption <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
