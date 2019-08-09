8819245ee064441a5929c6a62584bbab33d02950
preethical <preethi@deobald.ca>
2019-08-09 14:00:32 -0300

    moved global changes converting time to strptime and creating new column date to assignment utilities

--- Assignment1_utilities.R
+++ Assignment1_utilities.R
@@ -10,8 +10,13 @@ power_consumption$Date <- as.Date(power_consumption$Date, "%d/%m/%Y")
 
 ## Subset the required dates
 Power_subset <- subset(power_consumption, Date == "2007-02-01" | Date == "2007-02-02")
+
 ##other ways to subset dates
 ##Power_consumption <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
 ##Power_subset <- power_consumption[power_consumption$Date %in% c("2007-02-01","2007-02-02"),]
 ##Power_subset <- power_consumption %>% filter(between( Date, 2007-02-01, 2007-02-02))
 ##Sub_set <- power_consumption[(power_consumption$Date >= "2007-02-01") & (power_consumption$Date <= "2007-02-02")]
+
+Power_subset$datetime <- strptime(paste(Power_subset$Date, Power_subset$Time), "%Y-%m-%d %H:%M:%S")
+
+Power_subset$datetime <- as.POSIXct(Power_subset$datetime)
